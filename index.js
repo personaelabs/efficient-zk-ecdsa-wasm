c2744e2cd3c014788be2f861cd41353004034f47
Daniel Tehrani <contact@dantehrani.com>
2022-10-11 17:44:43 +0900

    Remove js related files for now

--- index.js
+++
@@ -1,7 +0,0 @@
-import pcg from "./pkg/rust_elliptic_curve.js";
-
-const main = () => {
-    console.log(pcg)
-    const point_row = "0479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8"
-    const result = pcg.compute_powers(point_row);
-}
