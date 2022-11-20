c2c9604ef21c4dd9e29bdb3a9f13808946a6e4c2
Daniel Tehrani <contact@dantehrani.com>
2022-11-20 23:50:28 +0100

    Add wasm compile for nodejs

---
+++ build/nodejs/rust_elliptic_curve.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} point_row
+* @returns {string}
+*/
+export function compute_powers(point_row: string): string;
+/**
+* @param {string} point
+* @param {string} powers
+* @returns {boolean}
+*/
+export function verify_powers(point: string, powers: string): boolean;
